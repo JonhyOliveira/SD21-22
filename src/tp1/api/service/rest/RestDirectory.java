cdc06e4d4de16d6d0b447a496ba63ff298d89a22
Joao Oliveira <jfv.oliveira@campus.fct.unl.pt>
2022-05-29 18:14:50 +0100

    Revert "Suporte para versoes"
    This reverts commit a3feac9813d2ef87badbf8b3a4def7b52900be29.

--- src/tp1/api/service/rest/RestDirectory.java
+++ src/tp1/api/service/rest/RestDirectory.java
@@ -2,7 +2,15 @@ package tp1.api.service.rest;
 
 import java.util.List;
 
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import tp1.api.FileInfo;
 
@@ -16,7 +24,6 @@ public interface RestDirectory {
 	public static final String PASSWORD = "password";
 	public static final String ACC_USER_ID = "accUserId";
 	public static final String USER_ID_SHARE = "userIdShare";
-	public static final String VERSION_HEADER = "X-DFS-Versao";
 
 	/**
 	 * Write a new version of a file. If the file exists, its contents are
@@ -24,13 +31,11 @@ public interface RestDirectory {
 	 * 
 	 * A file resource will has the full path "userId/filename".
 	 * 
-	 *
-	 * @param version
 	 * @param filename - name of the file.
 	 * @param data     - contents of the file.
 	 * @param userId   - id of the user.
 	 * @param password - the password of the user.
-	 *
+	 * 
 	 * @return 200 if success + FileInfo representing the file. 404 if the userId
 	 *         does not exist. 403 if the password is incorrect. 400 otherwise.
 	 */
@@ -38,26 +43,24 @@ public interface RestDirectory {
 	@Path("/{" + USER_ID + "}/{" + FILENAME + "}")
 	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
 	@Produces(MediaType.APPLICATION_JSON)
-	FileInfo writeFile(@HeaderParam(VERSION_HEADER) Long version, @PathParam(FILENAME) String filename, byte[] data, @PathParam(USER_ID) String userId,
-					   @QueryParam(PASSWORD) String password);
+	FileInfo writeFile(@PathParam(FILENAME) String filename, byte[] data, @PathParam(USER_ID) String userId,
+			@QueryParam(PASSWORD) String password);
 
 	/**
 	 * Delete an existing file ("userId/filename"). Only the owner (userId) can
 	 * delete the file.
 	 * 
-	 *
-	 * @param version
 	 * @param filename - name of the file.
 	 * @param userId   - id of the user.
 	 * @param password - the password of the user.
-	 *
+	 * 
 	 * @return 204 if success; 404 if the userId or filename does not exist. 403 if
 	 *         the password is incorrect. 400 otherwise.
 	 */
 	@DELETE
 	@Path("/{" + USER_ID + "}/{" + FILENAME + "}")
-	void deleteFile(@HeaderParam(VERSION_HEADER) Long version, @PathParam(FILENAME) String filename, @PathParam(USER_ID) String userId,
-					@QueryParam(PASSWORD) String password);
+	void deleteFile(@PathParam(FILENAME) String filename, @PathParam(USER_ID) String userId,
+			@QueryParam(PASSWORD) String password);
 
 	/**
 	 * Share the file "userId/filename" with another user. Only the owner (userId)
@@ -67,20 +70,18 @@ public interface RestDirectory {
 	 * userId/filename exist and password is correct (note: if userIdShare already
 	 * has access to the file, the operation still succeeds).
 	 * 
-	 *
-	 * @param version
 	 * @param filename    - name of the file.
 	 * @param userId      - id of the user.
 	 * @param userIdShare - id of the user to share the file with.
 	 * @param password    - the password of the user.
-	 *
+	 * 
 	 * @return 204 if success; 404 if the userId or userIdShare or filename does not
 	 *         exist. 403 if the password is incorrect. 400 otherwise.
 	 */
 	@POST
 	@Path("/{" + USER_ID + "}/{" + FILENAME + "}/share/{" + USER_ID_SHARE + "}")
-	void shareFile(Long version, @PathParam(FILENAME) String filename, @PathParam(USER_ID) String userId,
-				   @PathParam(USER_ID_SHARE) String userIdShare, @QueryParam(PASSWORD) String password);
+	void shareFile(@PathParam(FILENAME) String filename, @PathParam(USER_ID) String userId,
+			@PathParam(USER_ID_SHARE) String userIdShare, @QueryParam(PASSWORD) String password);
 
 	/**
 	 * Unshare the file "userId/filename" with another user. Only the owner (userId)
@@ -90,19 +91,18 @@ public interface RestDirectory {
 	 * userId/filename exist and password is correct (note: if userIdShare does not
 	 * have access to the file, the operation still succeeds).
 	 * 
-	 * @param version
 	 * @param filename    - name of the file.
 	 * @param userId      - id of the user.
 	 * @param userIdShare - id of the user to unshare the file with.
 	 * @param password    - the password of the user.
-	 *
+	 * 
 	 * @return 204 if success; 404 if the userId or userIdShare or filename does not
 	 *         exist. 403 if the password is incorrect. 400 otherwise.
 	 */
 	@DELETE
 	@Path("/{" + USER_ID + "}/{" + FILENAME + "}/share/{" + USER_ID_SHARE + "}")
-	void unshareFile(@HeaderParam(VERSION_HEADER) Long version, @PathParam(FILENAME) String filename, @PathParam(USER_ID) String userId,
-					 @PathParam(USER_ID_SHARE) String userIdShare, @QueryParam(PASSWORD) String password);
+	void unshareFile(@PathParam(FILENAME) String filename, @PathParam(USER_ID) String userId,
+			@PathParam(USER_ID_SHARE) String userIdShare, @QueryParam(PASSWORD) String password);
 
 	/**
 	 * Get the contents of the file "userId/filename". Who can read a file: the
@@ -110,13 +110,11 @@ public interface RestDirectory {
 	 * 
 	 * This operation should be implemented using HTTP redirect on success.
 	 * 
-	 *
-	 * @param version
 	 * @param filename  - name of the file.
 	 * @param userId    - id of the user.
 	 * @param accUserId - id of the user executing the operation.
 	 * @param password  - the password of accUserId.
-	 *
+	 * 
 	 * @return 200 if success + contents (through redirect to the File server); 404
 	 *         if the userId or filename or accUserId does not exist. 403 if the
 	 *         password is incorrect or the user cannot access the file. 400
@@ -125,27 +123,26 @@ public interface RestDirectory {
 	@GET
 	@Path("/{" + USER_ID + "}/{" + FILENAME + "}")
 	@Produces(MediaType.APPLICATION_OCTET_STREAM)
-	byte[] getFile(@HeaderParam(VERSION_HEADER) Long version, @PathParam(FILENAME) String filename, @PathParam(USER_ID) String userId,
-				   @QueryParam(ACC_USER_ID) String accUserId, @QueryParam(PASSWORD) String password);
+	byte[] getFile(@PathParam(FILENAME) String filename, @PathParam(USER_ID) String userId,
+			@QueryParam(ACC_USER_ID) String accUserId, @QueryParam(PASSWORD) String password);
 
 	/**
 	 * List the files a given user ("userId") has access to - this includes both its
 	 * own files and the files shared with her.
 	 * 
-	 * @param version
 	 * @param userId   - id of the user.
 	 * @param password - the password of the user.
-	 *
+	 * 
 	 * @return 200 if success + list of FileInfo; 404 if the userId does not exist.
 	 *         403 if the password is incorrect. 400 otherwise.
 	 */
 	@GET
 	@Path("/{" + USER_ID + "}")
 	@Produces(MediaType.APPLICATION_JSON)
-	List<FileInfo> lsFile(@HeaderParam(VERSION_HEADER) Long version, @PathParam(USER_ID) String userId, @QueryParam(PASSWORD) String password);
+	List<FileInfo> lsFile(@PathParam(USER_ID) String userId, @QueryParam(PASSWORD) String password);
 
 	@DELETE
 	@Path("{" + USER_ID + "}")
-	void deleteUserFiles(@HeaderParam(VERSION_HEADER) Long version, @PathParam(USER_ID) String userId, @QueryParam(PASSWORD) @DefaultValue("") String password, @QueryParam(TOKEN) String token);
+	void deleteUserFiles(@PathParam(USER_ID) String userId, @QueryParam(PASSWORD) @DefaultValue("") String password, @QueryParam(TOKEN) String token);
 
 }
