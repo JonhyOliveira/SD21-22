46a62d204ee71440f66fa3f34602e29af779e10e
Joao Oliveira <jfv.oliveira@campus.fct.unl.pt>
2022-05-30 12:01:56 +0100

    User delete com kafka

--- src/tp1/impl/servers/common/kafka/JavaDirectoryKafka.java
+++ src/tp1/impl/servers/common/kafka/JavaDirectoryKafka.java
@@ -2,15 +2,33 @@ package tp1.impl.servers.common.kafka;
 
 import tp1.api.service.java.Result;
 import tp1.impl.servers.common.JavaDirectory;
+import tp1.impl.servers.common.kafka.operations.OperationProcessor;
+import tp1.impl.servers.common.kafka.operations.UsersOperations;
+import util.kafka.KafkaSubscriber;
+
+import java.util.List;
+import java.util.logging.Logger;
 
 public class JavaDirectoryKafka extends JavaDirectory {
 
+    final static Logger Log = Logger.getLogger(JavaDirectoryKafka.class.getName());
+
+    protected final OperationProcessor operationProcessor = new OperationProcessor();
+
     public JavaDirectoryKafka() {
         super();
+
+        operationProcessor.registerOperationHandler(UsersOperations.DELETE.generateOperationHandler(userId -> {
+            Log.fine(String.format("User %s deleted, updating cache..", userId));
+            super.deleteUserFiles(userId, "", "");
+        }));
+
+        KafkaSubscriber.createSubscriber("kafka:9092", List.of(UsersOperations.NAMESPACE), "earliest")
+                .start(false, operationProcessor);
     }
 
     @Override
     public Result<Void> deleteUserFiles(String userId, String password, String token) {
-        return Result.error(Result.ErrorCode.FORBIDDEN); // disabled, done through kafka
+        return Result.error(Result.ErrorCode.FORBIDDEN, "Disabled"); // disabled, done through kafka
     }
 }
--- src/tp1/impl/servers/common/kafka/JavaUsersKafka.java
+++ src/tp1/impl/servers/common/kafka/JavaUsersKafka.java
@@ -3,8 +3,16 @@ package tp1.impl.servers.common.kafka;
 import tp1.api.User;
 import tp1.api.service.java.Result;
 import tp1.impl.servers.common.JavaUsers;
+import tp1.impl.servers.common.kafka.operations.UsersOperations;
+import util.kafka.KafkaPublisher;
+
+import java.util.logging.Logger;
 
 public class JavaUsersKafka extends JavaUsers {
+
+    final static Logger Log = Logger.getLogger(JavaUsersKafka.class.getName());
+
+    protected final KafkaPublisher publisher = KafkaPublisher.createPublisher("kafka:9092");
 
     public JavaUsersKafka() {
         super();
@@ -12,6 +20,12 @@ public class JavaUsersKafka extends JavaUsers {
 
     @Override
     public Result<User> deleteUser(String userId, String password) {
-        return super.deleteUser(userId, password);
+        var r = super.deleteUser(userId, password);
+
+        if (r.isOK())
+            publisher.publish(UsersOperations.DELETE.generateOperation(userId));
+
+        return r;
     }
+
 }
